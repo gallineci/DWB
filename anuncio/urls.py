0342de71d87f2c8454d769596c5b10b7b96082c0
gallineci <necigalli@gmail.com>
2025-05-27 13:06:07 -0300

    add novo.html (cadastro de anuncio)

--- anuncio/urls.py
+++ anuncio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListarAnuncios
+from .views import ListarAnuncios, CriarAnuncio
 
 urlpatterns = [
     path('', ListarAnuncios.as_view(), name='listar-anuncios'),
+    path('novo/', CriarAnuncio.as_view(), name='cadastrar=-anuncio'),
 ]
--- anuncio/views.py
+++ anuncio/views.py
@@ -1,7 +1,16 @@
 from django.views.generic import ListView
 from .models import Anuncio
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .forms import FormularioAnuncio
 
 class ListarAnuncios(ListView):
     model = Anuncio
     template_name = 'anuncio/listar.html'
     context_object_name = 'anuncios'
+
+class CriarAnuncio(CreateView):
+    model = Anuncio
+    form_class = FormularioAnuncio
+    template_name = 'anuncio/novo.html'
+    success_url = reverse_lazy('listar-anuncios')
