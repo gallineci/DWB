2bc007c4c063c57e4698081a663087de9735a218
gallineci <necigalli@gmail.com>
2025-05-28 12:48:01 -0300

    delecao e edicao de anuncios habilitada

--- anuncio/urls.py
+++ anuncio/urls.py
@@ -4,4 +4,6 @@ from anuncio.views import *
 urlpatterns = [
     path('', ListarAnuncios.as_view(), name='listar-anuncios'),
     path('novo/', CriarAnuncio.as_view(), name='cadastrar-anuncio'),
+    path('editar/<int:pk>/', EditarAnuncio.as_view(), name='editar-anuncio'),
+    path('deletar/<int:pk>/', DeletarAnuncio.as_view(), name='deletar-anuncio'),
 ]
--- anuncio/views.py
+++ anuncio/views.py
@@ -4,6 +4,7 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from .forms import FormularioAnuncio
 from django.views import View
+from django.views.generic.edit import UpdateView, DeleteView
 
 class ListarAnuncios(ListView):
     model = Anuncio
@@ -19,3 +20,15 @@ class CriarAnuncio(CreateView):
     def form_valid(self, form):
         form.instance.anunciante = self.request.user
         return super().form_valid(form)
+    
+
+class EditarAnuncio(UpdateView):
+    model = Anuncio
+    form_class = FormularioAnuncio
+    template_name = 'anuncio/editar.html'
+    success_url = reverse_lazy('listar-anuncios')
+
+class DeletarAnuncio(DeleteView):
+    model = Anuncio
+    template_name = 'anuncio/deletar.html'
+    success_url = reverse_lazy('listar-anuncios')
