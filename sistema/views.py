929977da4426223b396d930a1c271a15f6791611
gallineci <necigalli@gmail.com>
2025-05-28 21:25:13 -0300

    api com email

--- sistema/views.py
+++ sistema/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views.generic import View
 from django.contrib.auth import authenticate, login, logout
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 class Login (View):
     def get (self, request):
@@ -28,3 +31,21 @@ class Logout (View):
     def get(self, request):
         logout(request)
         return redirect (settings.LOGIN_URL)
+    
+class LoginAPI(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data,
+            context={
+                'request': request
+            }
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'id': user.id,
+            'nome': user.first_name,
+            'email': user.email,
+            'token': token.key
+        })
